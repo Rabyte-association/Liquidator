f9b23d8ed27619e688cda6fa05d6fcb41c76dcfd
Minusplus <mateuszsp231@gmail.com>
2022-10-27 23:35:18 +0200

    changed laggy xiao code

--- hoverdoard_serial/hoverdoard_serial.ino
+++ hoverdoard_serial/hoverdoard_serial.ino
@@ -1,9 +1,3 @@
-// Further information on https://github.com/bipropellant
-//
-// To run this code you need this liblary: https://github.com/bipropellant/bipropellant-hoverboard-api
-// but, to install this lib, you also need this one: https://github.com/bipropellant/bipropellant-protocol
-// I recomed just copying the .c nad .h files form protocol lib, to api lib, then copying the whole folder
-// into your aeduino libs folder.
 
 #include <HoverboardAPI.h>
 
@@ -15,7 +9,7 @@ HoverboardAPI hoverboard = HoverboardAPI(serialWrapper);
 float movement = 0;
 float turn = 0;
 float movement_current;
-float a;
+float a=1; //accel/decel speed  <10 
 
 void setup() {
   Serial1.begin(115200);
@@ -32,25 +26,26 @@ void loop() {
     if (data == 'b') {
       turn = Serial.parseInt();
     }
-    Serial.println(data);
-    if (data == 'p') {
-      a = Serial.parseFloat();
-    }
   }
 
   if (movement - movement_current > 0) {
-    //    movement_current += a * 1/sqrt(abs(movement - movement_current));
-    movement_current += a * 1 / (movement - movement_current);
+    //movement_current += a * 1/sqrt(abs(movement - movement_current));
+    movement_current += a;
+   // if(movement - movement_current > 20){  //trzeba wytestowac
+    //  movement_current+=5;
+    //}
   }
   if (movement - movement_current < 0) {
 
-    //    movement_current += -a * 1/sqrt(abs(movement - movement_current));
-    movement_current += -a * 1 / (movement - movement_current);
+    //movement_current += -a * 1/sqrt(abs(movement - movement_current));
+    movement_current += -a;
+    //if(movement - movement_current < -20){ //trzeba wytestowac
+    //  movement_current-=5;
+    //}
   }
 
-  Serial.print(movement);
-  Serial.print(", ");
+  Serial.print(turn);
+  Serial.print(" ");
   Serial.println(movement_current);
   hoverboard.sendPWM(movement_current, turn, PROTOCOL_SOM_NOACK);
-  delay(10);
 }
