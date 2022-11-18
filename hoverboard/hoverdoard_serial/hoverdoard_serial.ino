c851232b6ae04ae73d07a0f724b83a4e724f9421
Andrzej Kalinowski <jekrekk75@gmail.com>
2022-11-18 22:56:14 +0100

    disabled acceleration

--- hoverboard/hoverdoard_serial/hoverdoard_serial.ino
+++ hoverboard/hoverdoard_serial/hoverdoard_serial.ino
@@ -20,7 +20,7 @@ void loop() {
   if (Serial.available() > 1) {
 
     byte data = Serial.read();
-    if (data == 'a') {
+    if (data == 'x') {
       movement = Serial.parseInt();
     }
     if (data == 'b') {
@@ -28,24 +28,25 @@ void loop() {
     }
   }
 
-  if (movement - movement_current > 0) {
-    //movement_current += a * 1/sqrt(abs(movement - movement_current));
-    movement_current += a;
-   // if(movement - movement_current > 20){  //trzeba wytestowac
-    //  movement_current+=5;
-    //}
-  }
-  if (movement - movement_current < 0) {
+//  if (movement - movement_current > 0) {
+//    //movement_current += a * 1/sqrt(abs(movement - movement_current));
+//    movement_current += a;
+//   // if(movement - movement_current > 20){  //trzeba wytestowac
+//    //  movement_current+=5;
+//    //}
+//  }
+//  if (movement - movement_current < 0) {
+//
+//    //movement_current += -a * 1/sqrt(abs(movement - movement_current));
+//    movement_current += -a;
+//    //if(movement - movement_current < -20){ //trzeba wytestowac
+//    //  movement_current-=5;
+//    //}
+//  }
 
-    //movement_current += -a * 1/sqrt(abs(movement - movement_current));
-    movement_current += -a;
-    //if(movement - movement_current < -20){ //trzeba wytestowac
-    //  movement_current-=5;
-    //}
-  }
-
-  Serial.print(turn);
-  Serial.print(" ");
-  Serial.println(movement_current);
-  hoverboard.sendPWM(movement_current, turn, PROTOCOL_SOM_NOACK);
+//  Serial.print(turn);
+//  Serial.print(" ");
+//  Serial.println(movement_current);
+//  hoverboard.sendPWM(movement_current, turn, PROTOCOL_SOM_NOACK);
+  hoverboard.sendPWM(movement, turn, PROTOCOL_SOM_NOACK);
 }
